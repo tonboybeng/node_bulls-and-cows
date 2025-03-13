4a0b7e4150d6be9adb7725996c47a6d3f0c0cb5f
Toni_Adrenalin <toni.toni@adrenalinmedia.com.au>
2025-03-13 21:15:59 +1100

    Solution

--- src/app.js
+++ src/app.js
@@ -11,22 +11,12 @@ const rl = readline.createInterface({ input, output });
 
 const question1 = 'Please guess a number';
 const question2 = '(must be 4 digits number and each digit is unique number): ';
-const error1 = 'Invalid guess.';
-const error2 = 'Must be 4 digits number and each digit is unique number.';
 
 rl.question(`${question1} ${question2}`, (answer) => {
   if (checkIsValidUserInput(answer)) {
     const numberGenerated = generateRandomNumber();
 
-    const result = getBullsAndCows(+answer, numberGenerated);
-
-    console.log(`Your guess: ${answer}. Generated number: ${numberGenerated}`);
-
-    console.log(`Your result: `);
-
-    console.log(result);
-  } else {
-    console.log(`${error1} ${error2}`);
+    getBullsAndCows(+answer, numberGenerated);
   }
 
   rl.close();
